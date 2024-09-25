e005b93014764cc9fa07925ecd51a63baae35eb5
Emilia Fernández <11emiliafernandez@gmail.com>
2024-09-25 10:27:25 -0300

    Se agregó acceso a la sección "Documentation"

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized") 
+
+driver = webdriver.Chrome(options=options)
 
 driver.get("https://google.com")
       
@@ -36,8 +39,19 @@ print("El título de la página es correcto:", driver.title)
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 time.sleep(2) 
 driver.execute_script("window.scrollTo(0, 0);")
+time.sleep(2) 
 print("Desplazamiento completado.")
 
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.LINK_TEXT, "Documentation"))
+)
+
+try:
+    doc_link = driver.find_element(By.LINK_TEXT, "Documentation")
+    doc_link.click()
+    print("Accediendo a la documentación.")
+except Exception as e:
+    print(f"No se pudo encontrar el enlace de 'Documentation': {e}")
 
 time.sleep(3)
 driver.quit()
